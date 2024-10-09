1cee0bfa60f541f4a0b099d349b86fdd752bc683
Cristian Coronel <cristiancoronel@wposs.com>
2024-10-09 11:09:32 -0500

    Modify_Obtained_JSON_Object
    - Se modifica el objeto JSON que se obtiene para cambiar de JSON_ARRAY a JSON_OBJECT

--- app/src/main/java/com/cj/crstats/model/CRApiService.kt
+++ app/src/main/java/com/cj/crstats/model/CRApiService.kt
@@ -3,7 +3,6 @@ package com.cj.crstats.model
 import com.cj.crstats.model.dataobj.PlayerDataObj
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Path
 
 interface CRApiService {
@@ -12,6 +11,6 @@ interface CRApiService {
     fun getPlayerInfo(@Header("Authorization") authorization: String, @Path("playerTag") playerTag: String) : Call<List<PlayerDataObj>>*/
 
     @GET("players/{playerTag}")
-    fun getPlayerInfo(@Path("playerTag") playerTag: String) : Call<List<PlayerDataObj>>
+    fun getPlayerInfo(@Path("playerTag") playerTag: String) : Call<PlayerDataObj>
 
 }
