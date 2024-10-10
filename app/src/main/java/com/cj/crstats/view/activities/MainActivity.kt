9fda1f083d7fbc57504d522e1398c66aaaa40a8b
JoelNavarroV <juanjoeln8@gmail.com>
2024-10-10 10:07:23 -0500

    main_interfaz
    - Se añaden recursos para uso futuro.
    - Se implementa apariencia de interfaz principal.
    - Se añade buscador personalizado.

--- app/src/main/java/com/cj/crstats/view/activities/MainActivity.kt
+++ app/src/main/java/com/cj/crstats/view/activities/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.cj.crstats.view.activities
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cj.crstats.R
+import com.cj.crstats.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
